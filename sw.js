e82e8aa095b85b59c3b5fbec80a5b796d2a96c4b
George <95352129+Jeorgeskam@users.noreply.github.com>
2023-12-09 20:15:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,57 @@
+// sw.js
+
+const CACHE_NAME = 'simple-dtmf-dialer-cache';
+const urlsToCache = [
+  '/',
+  'about-icon.png',
+  'blobs-l.svg',
+  'dialer-background-image.png',
+  '1.png',
+  '2.png',
+  '3.png',
+  'call.png',
+  '4.png',
+  '5.png',
+  '6.png',
+  'end_call.png',
+  '7.png',
+  '8.png',
+  '9.png',
+  'hold.png',
+  'star.png',
+  '0.png',
+  'hash.png',
+  'transfer.png',
+  'icon.png',
+  'index.html',
+  'sw.js',
+  'manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('simple-dtmf-tone-generator-web-app').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/sw.js',
-        '/1.png',
-        '/2.png',
-        '/3.png',
-        '/4.png',
-        '/5.png',
-        '/6.png',
-        '/7.png',
-        '/8.png',
-        '/9.png',
-        '/0.png',
-        '/about-icon.png',
-        '/blobs-l.svg',
-        '/call.png',
-        '/dialer-background-image.png',
-        '/end_call.png',
-        '/hash.png',
-        '/hold.png',
-        '/transfer.png',
-        '/star.png',
-        '/icon.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
