1694ad6bee366b5b24a3a3b6497a531135d1479e
George <95352129+Jeorgeskam@users.noreply.github.com>
2023-12-09 20:09:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,40 @@
-{
-  "name": "DTMF Dialer",
-  "short_name": "DTMF Dialer",
-  "description": "A Simple DTMF Dialer",
-  "start_url": "/",
-  "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#000000",
-  "icons": [
-    {
-      "src": "/icon.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
-}
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('simple-dtmf-tone-generator-web-app').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/sw.js',
+        '/1.png',
+        '/2.png',
+        '/3.png',
+        '/4.png',
+        '/5.png',
+        '/6.png',
+        '/7.png',
+        '/8.png',
+        '/9.png',
+        '/0.png',
+        '/about-icon.png',
+        '/blobs-l.svg',
+        '/call.png',
+        '/dialer-background-image.png',
+        '/end_call.png',
+        '/hash.png',
+        '/hold.png',
+        '/transfer.png',
+        '/star.png',
+        '/icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
